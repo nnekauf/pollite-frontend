ff96f1224a614d07a2984b8eda9e794aed132399
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-05 19:57:32 -0400

    updates the render with the router and store

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router >
+    <Provider store={store}>
+        <App/>
+    </Provider>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
