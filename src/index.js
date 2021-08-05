b421abbb5ad37485784b6f2a4e57ab626c3e9c1f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-05 19:40:32 -0400

    updates imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import store from './store.js'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
