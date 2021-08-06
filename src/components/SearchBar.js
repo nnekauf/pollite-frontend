08dbfbb045aceb171c343cabd84bf5cc718d1114
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 19:42:31 -0400

    adds form and event handler method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { getAllRepresentatives } from '../actions/representatives'
 
 const SearchBar = ({}) => {
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        getAllRepresentatives(zipcode)
+
+    }
+    
     return (
         <>
-        form 
+        <form onSubmit={handleSubmit}>
+            <input placeholder= "Enter Zipcode" value = {zipcode} onChange={handleChange}/>  
+            < input type = "submit" value = "Submit" />
+        </form>
         </>
     )
 }
