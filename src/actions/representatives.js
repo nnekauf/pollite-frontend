ed019216d7a0aac1a44fba7bb2cbdfe755f420d0
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 19:27:06 -0400

    adds getAllRepresentatives to actions

--- src/actions/representatives.js
+++ src/actions/representatives.js
@@ -5,3 +5,26 @@ export const showAllRepresentatives = representatives => {
         representatives
     }
 }
+
+export const getAllRepresentatives = (zipcode) => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/representatives/", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(zipcode)
+        })
+        .then (response => response.json())
+        .then (r => {
+            if (r.error) {
+                alert(r.error)
+            } else {
+                dispatch(showAllRepresentatives(r))
+    
+            }
+        })
+        .catch(console.log)
+    }
+}
