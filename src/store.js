89fef9cba06f0f502234697d28d1234e2904077a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-05 20:01:30 -0400

    creates store file to keep tract of the state of the representatives

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+const combinedReducer = combineReducers({
+  representatives, 
+ 
+ 
+})
+
+const store = createStore(combinedReducer, compose(applyMiddleware(thunk), composeWithDevTools()))
+
+
+export default store
